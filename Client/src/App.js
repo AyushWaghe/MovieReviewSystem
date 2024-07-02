ad9f9ee116f69661bbdf8e76daaa5bb372ccf320
Sambhav Kuamth <sambhavkumath1357@gmail.com>
2024-07-02 20:30:21 +0530

    dataset created

--- Client/src/App.js
+++ Client/src/App.js
@@ -18,16 +18,24 @@ function App() {
       <header className="App-header">
         <h1>Movie Review Website</h1>
       </header>
-      <div className="movie-container">
-        {movies.map((movie) => (
-          <div className="movie-card" key={movie.id} onClick={() => handleCardClick(movie)}>
-            <img src={process.env.PUBLIC_URL + '/' + movie.poster} alt={movie.title} className="movie-image" />
-            <div className="movie-title">{movie.title}</div>
-            <div className="movie-hover-details">
-              <p><strong>Summary:</strong> {movie.summary}</p>
+      <div className="container">
+        <div className="left-column">
+          {/* Add content for the left column here */}
+        </div>
+        <div className="movie-container">
+          {movies.map((movie) => (
+            <div className="movie-card" key={movie.id} onClick={() => handleCardClick(movie)}>
+              <img src={process.env.PUBLIC_URL + '/' + movie.poster} alt={movie.title} className="movie-image" />
+              <div className="movie-title">{movie.title}</div>
+              <div className="movie-hover-details">
+                <p><strong>Summary:</strong> {movie.summary}</p>
+              </div>
             </div>
-          </div>
-        ))}
+          ))}
+        </div>
+        <div className="right-column">
+          {/* Add content for the right column (filter bar) here */}
+        </div>
       </div>
       {selectedMovie && (
         <div className="modal" onClick={handleClosePopup}>
@@ -38,7 +46,7 @@ function App() {
             <p><strong>Duration:</strong> {selectedMovie.duration}</p>
             <p><strong>Average Stars:</strong> {'★'.repeat(selectedMovie.stars)}</p>
             <p><strong>Summary:</strong> {selectedMovie.summary}</p>
-            <p><strong>Year of Release:</strong> {selectedMovie.year}</p>
+            <p><strong>Year of Release:</strong> {selectedMovie.release_year}</p>
             <p><strong>Language:</strong> {selectedMovie.language}</p> {/* Add this line */}
           </div>
         </div>
