cbe26926709495540b94e51211bfc1e65ed7bf6a
Greeshma_Bellamkonda1 <greeshma1612475@gmail.com>
2024-07-03 12:53:43 +0530

    Update

--- Client/src/Views/HomePage/HomePage.js
+++ Client/src/Views/HomePage/HomePage.js
@@ -1,29 +1,16 @@
-// client/src/components/HomePage.js
 import React from 'react';
 import MovieList from '../../components/MovieCard/MovieList';
 import Navbar from '../../components/NavBar/navBar';
 
-import './HomePage.css';
-
 const HomePage = () => {
   return (
     <div>
-        <div className="HomePage">
       <Navbar />
-      <div className="home-page">
-        <div className="background-image">
-          <div className="DarkOverlayH"></div>
-          <MovieList />
-        </div>
-      </div>
-      
-      </div>
-    
+      <MovieList />
     </div>
   );
 };
 
 export default HomePage;
-
 
      
