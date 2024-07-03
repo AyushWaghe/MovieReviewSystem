c2bb54438d0751ed9143529a287d35cd541966a7
Greeshma_Bellamkonda1 <greeshma1612475@gmail.com>
2024-07-03 12:54:16 +0530

    Update

--- Client/src/components/MovieCard/MovieList.js
+++ Client/src/components/MovieCard/MovieList.js
@@ -1,10 +1,7 @@
-// client/src/components/MovieList.js
 import React, { useState, useEffect } from 'react';
 import MovieCard from './MovieCard';
 import moviesData from '../../moviesData';
 import './MovieList.css';
-import Navbar from '../NavBar/navBar';
-
 
 const MovieList = () => {
   const [movies, setMovies] = useState([]);
@@ -15,16 +12,13 @@ const MovieList = () => {
   }, []);
 
   return (
-    <div>
-      <Navbar />
-      <div className="movie-list">
-        <div className="HomeDiv">
-          <div className="DarkOverlayM">
-            <div className="movie-container">
-              {movies.map(movie => (
-                <MovieCard key={movie.id} movie={movie} />
-              ))}
-            </div>
+    <div className="movie-list">
+      <div className="HomeDiv">
+        <div className="DarkOverlayM">
+          <div className="movie-container">
+            {movies.map(movie => (
+              <MovieCard key={movie.id} movie={movie} />
+            ))}
           </div>
         </div>
       </div>
